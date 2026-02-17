f9826d3779e7eabf62606b3c0ce212363457fdbd
zulpikar <zulpikar@example.com>
2026-02-17 17:23:06 +0700

    UI: Update Click Animation (Text & Random Color)

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,14 @@ import './bootstrap';
 // Logika Animasi NG-LOCO
 document.addEventListener('click', function (e) {
     const text = document.createElement('div');
-    text.innerText = 'NG-LOCO';
-    // Ukuran teks lebih besar (text-4xl), warna merah terang
-    text.className = 'flying-text text-4xl font-bold text-red-500';
+    text.innerText = 'developed by garox dev';
+
+    // Random warna: Kuning atau Putih
+    const colors = ['text-yellow-500', 'text-white'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    // Ukuran teks lebih besar (text-4xl), warna random
+    text.className = `flying-text text-4xl font-bold ${randomColor}`;
 
     // Set posisi awal di kursor
     text.style.left = e.clientX + 'px';
